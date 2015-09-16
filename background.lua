3cededed878a94307c2b3f82a7c595fc3e1baa7d
unknown <uczjc@PUIP62140.ad.nottingham.ac.uk>
2015-09-16 11:22:08 +0100

    FourCorner style backgrounds working! Side effect of this is FourCorner can be used to create Horizontal, Vertical and corner positioned Rectangle gradients!

--- background.lua
+++ background.lua
@@ -31,8 +31,8 @@ Background.Meshes = {
 -- enum of styles
 Background.Styles = {
   Transparent = 0,
-  Solid = 1
---  FourCorner = 2,
+  Solid = 1,
+  FourCorner = 2
 --  Linear = 3,
 --  Radial = 4,
 --  Rectangle = 5,
@@ -54,6 +54,9 @@ function Background:draw(wnd)
   if self.style == self.Styles.Solid then
     self:drawSolid(wnd)
   end
+  if self.style == self.Styles.FourCorner then
+    self:drawFourCorner(wnd)
+  end
   --Transparent or anything else will just return as it met no case above!
 end
 
@@ -67,7 +70,31 @@ function Background:drawSolid(wnd)
   love.graphics.setColor(color)
   
   local mesh = self.Meshes.Internal.Base
-  if mesh:hasVertexColors then mesh:setVertexColors(false) end --don't use per-vertex colours!
+  if mesh:hasVertexColors() then mesh:setVertexColors(false) end -- don't use per-vertex colours!
+  
+  love.graphics.draw(mesh,
+    wnd.left, wnd.top, 0, wnd.width / 10, wnd.height / 10) -- don't like the magic numbers
+end
+
+function Background:drawFourCorner(wnd)
+  if type(self.color[1]) ~= "table" then --color has only one colour!
+    self.color = { self.color } --wrap it inside a table of colours!
+  end
+  
+  local mesh = self.Meshes.Internal.Base
+  if not mesh:hasVertexColors() then mesh:setVertexColors(true) end -- use per-vertex colours!
+  
+  -- set each vertex colour from the first 4 colours in self.color
+  for i = 1, 4 do
+    local x, y, u, v, r, g, b, a = mesh:getVertex(i)
+    if self.color[i] then
+      r, g, b, a = self.color[i][1], self.color[i][2], self.color[i][3], self.color[i][4]
+    else
+      r, g, b, a = 255, 255, 255, 255 -- default to white if unspecified (matches LOVE's Mesh conventions)
+    end
+    
+    mesh:setVertex(i, x, y, u, v, r, g, b, a)
+  end
   
   love.graphics.draw(mesh,
     wnd.left, wnd.top, 0, wnd.width / 10, wnd.height / 10) -- don't like the magic numbers
